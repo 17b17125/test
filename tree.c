ad563dd82cb40ea04c215d58f322fc0384c93f82
Sasagawa Takuya(ouchi) <sasagawa.t.ae@m.titech.ac.jp>
2018-07-24 08:18:20 +0900

    adding tree

---
+++ tree.c
@@ -0,0 +1,234 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct tree {
+  struct tree *l;
+  int v;
+  struct tree *r;
+  int h;
+};
+
+typedef struct tree tree_t;
+
+int count;
+int glb1;
+int glb2;
+int glb3;
+int glb4;
+
+int tree_height(tree_t *t){
+  if(t == NULL) return -1;
+  if(t->l == NULL && t->r == NULL) return 0;
+  if(t->l == NULL) return t->r->h + 1;
+  if(t->r == NULL) return t->l->h + 1;
+  return t->l->h > t->r->h ? t->l->h + 1 : t->r->h + 1;
+}
+
+/*
+  2分探索木 l の要素は必ず v より小さい
+  2分探索木 r の要素は必ず v より大きい
+*/
+tree_t *tree_create(tree_t *l, int v, tree_t *r){
+  tree_t *t = malloc(sizeof(tree_t));
+  count++;
+  t->l = l;
+  t->r = r;
+  t->v = v;
+  t->h = tree_height(l) > tree_height(r) ? tree_height(l) + 1 : tree_height(r) + 1;
+  return t;
+}
+
+void tree_free(tree_t *t){
+  if(t != NULL){
+    free(t);
+    count--;
+  }
+}
+
+int tree_size(tree_t *t){
+  if(t == NULL) return 0;
+  return tree_size(t->l) + tree_size(t->r) + 1;
+}
+
+void tree_print_elements(tree_t *t){
+  if(t == NULL){
+  }else{
+    if(t->l == NULL){
+      printf("%d ", t->v);
+      tree_print_elements(t->r);
+    }else{
+      tree_print_elements(t->l);
+      printf("%d ", t->v);
+      tree_print_elements(t->r);
+    }
+  }
+}
+
+/*
+  tree_height(l) <= tree_height(r) + 2 &&
+  tree_height(r) <= tree_height(l) + 2
+  が true であると仮定してよい
+*/
+
+//発展的課題
+tree_t *tree_bal(tree_t *l, int v, tree_t *r) {
+  tree_t *ll, *lr, *lrl, *lrr;
+  int lv, lrv;
+
+  if(tree_height(r) <= 1) return tree_create(l, v, r);
+  if(l == NULL) return tree_create(l, v, r);
+  ll = l->l;
+  lr = l->r;
+  lv = l->v;
+  if(tree_height(ll) >= tree_height(lr)) return tree_create(ll, lv, tree_create(lr, v, r));
+  lrl = lr->l;
+  lrr = lr->r;
+  lrv = lr->v;
+  return tree_create(tree_create(ll, lv, tree_create(lrl, lrv, lrr)), v, r);
+}
+
+
+int tree_mem(int x, tree_t *t){
+  if(t == NULL) return 0;
+  if(x == t->v) return 1;
+  if(x < t->v){
+    if(t->l == NULL) return 0;
+    return tree_mem(x, t->l);
+  }else{
+    if(t->r == NULL) return 0;
+    return tree_mem(x, t->r);
+  }
+}
+
+tree_t *tree_add(int x, tree_t *t) {
+  tree_t *tl,*tr;
+  int tv;
+
+  if(t == NULL) return tree_bal(NULL, x, NULL);
+  if(t->v == x) return t;
+  tl = t->l;
+  tr = t->r;
+  tv = t->v;
+  //tree_free(t);
+  if(x < tv){
+    return tree_bal(tree_add(x, tl), tv, tr);
+  }else{
+    return tree_bal(tl, tv, tree_add(x, tr));
+  }
+}
+
+int tree_min(tree_t *t){
+  if(t == NULL) return -1;
+  if(t->l != NULL) return tree_min(t->l);
+  return t->v;
+}
+
+int tree_max(tree_t *t){
+  if(t == NULL) return -1;
+  if(t->r != NULL) return tree_max(t->r);
+  return t->v;
+}
+
+tree_t *tree_remove_min(tree_t *t){
+  tree_t *tl,*tr;
+  int tv;
+
+  if(t == NULL) return NULL;
+  tl = t->l;
+  tr = t->r;
+  tv = t->v;
+  if(tl != NULL) {
+    //tree_free(t);
+    return tree_bal(tree_remove_min(tl), tv, tr);
+  }else{
+    return tr;
+  }
+}
+
+/*
+  2分探索木 t1 の要素は必ず2分探索木 t2 の要素より小さい
+*/
+tree_t *tree_merge(tree_t *t1, tree_t *t2){
+  tree_t *tmp1,*tmp2;
+
+  tmp1 = t1;
+  tmp2 = t2;
+  //tree_free(t1);
+  //tree_free(t2);
+  return tree_bal(tmp1, tree_min(tmp2), tree_remove_min(tmp2));
+}
+
+tree_t *tree_remove(int x, tree_t *t){
+  tree_t *tl, *tr;
+  int tv;
+
+  if(t == NULL) return NULL;
+  tl = t->l;
+  tr = t->r;
+  tv = t->v;
+  //tree_free(t);
+  if(x == tv) {
+    if(tr != NULL) return tree_bal(tl, tree_min(tr), tree_remove_min(tr));
+    return tl;
+  }
+  if(x < tv) return tree_bal(tree_remove(x, tl), tv, tr);
+  return tree_bal(tl, tv, tree_remove(x, tr));
+}
+
+
+//平衡性のチェック(発展的課題)
+int tree_check(tree_t *t){
+  if(t == NULL) return 1;
+  return tree_height(t->l) <= tree_height(t->r)+1 &&
+  tree_height(t->r) <= tree_height(t->l)+1 && tree_check(t->l) && tree_check(t->r);
+}
+
+
+int main(){
+  int i;
+  tree_t *t = NULL;
+  count = 0;
+
+  for(i = 0; i < 7919; i++){
+    t = tree_add(i*4000 % 7919, t);
+  }
+
+  //tree_print_elements(t);
+
+//勝手に追加した tree_remove_minのチェック
+  /*for(i = 0; i < 7919; i++){
+    if(tree_min(t) != i) printf("%d vs %d\n", i, tree_min(t));
+    t = tree_remove_min(t);
+  }
+  printf("removed_mins: size = %d\n", tree_size(t));
+  */
+
+// サイズが7919かチェック
+  printf("size = %d\n", tree_size(t));
+  for(i = 0; i < 7919; i++){
+    t = tree_remove(i*5000 % 7919, t);
+  }
+
+  tree_print_elements(t);
+// サイズが0かチェック
+  //printf("size = %d\n", tree_size(t));
+
+
+//  平衡性のチェック
+  for(i = 0; i < 100000; i++) {
+    int rnd = rand()%1000;
+    if(rand() % 2)
+      t = tree_remove(rnd, t);
+    else
+      t = tree_add(rnd, t);
+    if(!tree_check(t)) puts("ERROR");
+  }
+
+//  確保しているノードの領域の数と木のサイズが一致するかチェック
+  printf("count = %d\n", count);
+  printf("size = %d\n", tree_size(t));
+
+
+  return 0;
+}
